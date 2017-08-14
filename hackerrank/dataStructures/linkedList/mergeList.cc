6c100be1209c3479c65a170b8aced067d396ef5a
Philip Scott <pscott@zeptohost.com>
2017-08-14 16:22:20 -0400

    Refactor solution; now it looks prettier ;)

--- hackerrank/dataStructures/linkedList/mergeList.cc
+++ hackerrank/dataStructures/linkedList/mergeList.cc
@@ -1,29 +1,35 @@
 /*
-  Merge two sorted lists A and B as one linked list
-  Node is defined as 
-  struct Node
-  {
-     int data;
-     struct Node *next;
+   Merge two sorted lists A and B as one linked list
+   Node is defined as 
+   struct Node
+   {
+   int data;
+   struct Node *next;
+   }
+   */
+Node *MergeListsHelper(Node *head, Node *headA, Node *headB) {
+  if (headA == nullptr && headB == nullptr) return head;
+  else if (headA == nullptr) {
+    head = headB;
+    headA = headB = nullptr;
   }
-*/
-void MergeListsHelper(Node *head, Node *headA, Node *headB) {
-    if (headA == nullptr && headB == nullptr) return;
-    else if (headA == nullptr || (headB && headB->data < headA->data)) head->next = headB, headB = headB->next;
-    else if (headB == nullptr || (headA && headA->data < headB->data)) head->next = headA, headA = headA->next;
-    MergeListsHelper(head->next, headA, headB);
+  else if (headB == nullptr) {
+    head = headA;
+    headA = headB = nullptr;
+  }
+  else if (headB->data < headA->data) {
+    head = headB;
+    headB = headB->next;
+  }
+  else if (headA->data < headB->data) {
+    head = headA;
+    headA = headA->next;
+  }
+  head->next = MergeListsHelper(head->next, headA, headB);
+  return head;
 }
 Node* MergeLists(Node *headA, Node* headB) {
-    Node *newHead;
-    if (headA == nullptr && headB == nullptr) return nullptr;
-    if (headB == nullptr || (headA && headA->data < headB->data)) {
-        newHead = headA;
-        MergeListsHelper(newHead, headA->next, headB);
-    } else {
-        newHead = headB;
-        MergeListsHelper(newHead, headA, headB->next);
-    }
-    
-    return newHead;
+  Node *newHead = nullptr;
+  newHead = MergeListsHelper(newHead, headA, headB);
+  return newHead;
 }
-
