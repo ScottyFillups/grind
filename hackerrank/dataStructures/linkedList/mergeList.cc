1934c7388e5566f4d11a2b524c53281f360cae9f
Philip Scott <pscott@zeptohost.com>
2017-08-14 15:38:06 -0400

    Add correct solution

--- hackerrank/dataStructures/linkedList/mergeList.cc
+++ hackerrank/dataStructures/linkedList/mergeList.cc
@@ -9,13 +9,21 @@
 */
 void MergeListsHelper(Node *head, Node *headA, Node *headB) {
     if (headA == nullptr && headB == nullptr) return;
-    else if (head == nullptr) 
     else if (headA == nullptr || (headB && headB->data < headA->data)) head->next = headB, headB = headB->next;
     else if (headB == nullptr || (headA && headA->data < headB->data)) head->next = headA, headA = headA->next;
     MergeListsHelper(head->next, headA, headB);
 }
 Node* MergeLists(Node *headA, Node* headB) {
-    Node *newHead = nullptr;
-    MergeListsHelper(newHead, headA, headB);
+    Node *newHead;
+    if (headA == nullptr && headB == nullptr) return nullptr;
+    if (headB == nullptr || (headA && headA->data < headB->data)) {
+        newHead = headA;
+        MergeListsHelper(newHead, headA->next, headB);
+    } else {
+        newHead = headB;
+        MergeListsHelper(newHead, headA, headB->next);
+    }
+    
     return newHead;
 }
+
