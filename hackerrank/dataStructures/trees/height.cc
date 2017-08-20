918ad9c8c75397d4c7900272b3e2f405fc5338cf
Philip Scott <pscott@zeptohost.com>
2017-08-20 15:57:27 -0400

    Remove helper function

--- hackerrank/dataStructures/trees/height.cc
+++ hackerrank/dataStructures/trees/height.cc
@@ -1,21 +1,18 @@
-
-
 /*The tree node has data, left child and right child 
-class Node {
-    int data;
-    Node* left;
-    Node* right;
-};
+  class Node {
+  int data;
+  Node* left;
+  Node* right;
+  };
 
 */
-    int heightHelper (Node *root, int height) {
-        if (root == nullptr) return height;
-        int left = heightHelper(root->left, height + 1);
-        int right = heightHelper(root->right, height + 1);
-        return (left > right ? left : right);
-    }
-    int height(Node* root) {
-        // a null will be counted, so we take n - 1
-        return heightHelper(root, 0) - 1;
-    }
-  
+int height(Node* root) {
+  int left = 0;
+  int right = 0;
+
+  if (root == nullptr) return -1;
+  left = 1 + height(root->left);
+  right = 1 + height(root->right);
+
+  return (left > right ? left : right);
+}
