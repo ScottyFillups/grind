ea6e2b385a3e4a87da852a1361aa611a3019724a
Philip Scott <pscott@zeptohost.com>
2017-08-28 18:09:26 -0400

    Add tower-of-babel files

---
+++ nodeschool/tower-of-babel/exeModule.js
@@ -0,0 +1,8 @@
+import {PI, sqrt, square} from './module'
+
+var arg1 = parseFloat(process.argv[2])
+var arg2 = parseFloat(process.argv[3])
+
+console.log(PI)
+console.log(sqrt(arg1))
+console.log(square(arg2))
---
+++ nodeschool/tower-of-babel/exeModuleDefault.js
@@ -0,0 +1,8 @@
+import _Math from './moduleDefault'
+
+var arg1 = parseFloat(process.argv[2])
+var arg2 = parseFloat(process.argv[3])
+
+console.log(_Math.PI)
+console.log(_Math.sqrt(arg1))
+console.log(_Math.square(arg2))
