b815177cb1a4aecf363674551def35575f7f0eb2
Philip Scott <pscott@zeptohost.com>
2017-08-31 14:20:18 -0400

    Finish glsl intro

--- nodeschool/shader-school/06-intro-6/mpow.glsl
+++ nodeschool/shader-school/06-intro-6/mpow.glsl
@@ -1,13 +1,16 @@
 mat2 matrixPower(highp mat2 m, int n) {
-  
-  //Raise the matrix m to nth power
-
-  // For example:
-  //
-  //  matrixPower(m, 2) = m * m
-  //
-
-  return mat2(1.0);  
+  highp mat2 tmp = m;
+  if (n == 0) {
+    return mat2(1, 0,
+                0, 1);
+  }
+  for (int i = 1; i <= 16; i++) {
+    if (i == n) {
+      break;
+    }
+    tmp *= m;
+  }
+  return tmp;
 }
 
 //Do not change this line or the name of the above function
