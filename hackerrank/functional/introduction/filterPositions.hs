4930c34c160f14d306656bda8e4688b0d5d78652
Philip Scott <pscott@zeptohost.com>
2017-12-23 13:47:31 -0500

    Add alternate solution for filterPositions

--- hackerrank/functional/introduction/filterPositions.hs
+++ hackerrank/functional/introduction/filterPositions.hs
@@ -1,6 +1,10 @@
 f :: [Int] -> [Int]
 f lst = map snd (filter (\x -> even . fst $ x ) (zip [1..] lst))
 -- A more concise way: f = map snd . filter (odd . fst) . zip [0..]
+-- Another concise solution:
+-- f (_:x:xs) = x : f xs
+-- f _ = []
+
 
 -- This part deals with the Input and Output and can be used as it is. Do not modify it.
 main = do
