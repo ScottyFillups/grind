604bdc9087a3e24838e0d8a0cad1ed18f8d38007
Philip Scott <pscott@zeptohost.com>
2018-02-22 23:03:56 -0800

    Finish Vue intro

--- vue/intro/app.js
+++ vue/intro/app.js
@@ -12,7 +12,40 @@ const app2 = new Vue({
   }
 })
 
+const app3 = new Vue({
+  el: '#app-3',
+  data: {
+    todos: [
+      { text: 'Make cool stuff' },
+      { text: 'Get GitHub stars' },
+      { text: '???' },
+      { text: 'Profit!!!' }
+    ]
+  }
+})
 
-// setTimeout(() => {
-//   app1.message = 'Message changed!'
-// }, 2000)
+const app4 = new Vue({
+  el: '#app-4',
+  data: {
+    message: 'hello, world'
+  }
+})
+
+Vue.component('todo-list', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+const app5 = new Vue({
+  el: '#app-5',
+  data: {
+    todos: [
+      { id: 0, text: 'Loochan' },
+      { id: 1, text: 'Tutturu' },
+      { id: 2, text: '???' },
+      { id: 3, text: 'Profit!!!' }
+    ]
+  }
+})
+
+
