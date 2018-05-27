98e5c33528a4ee797c7581fb209f426850302829
Philip Scott <pscott@zeptohost.com>
2018-05-27 17:33:43 -0400

    feat(why): Add selection sort

--- fundamentals/sorting/bubble.go
+++ fundamentals/sorting/bubble.go
@@ -1,9 +1,5 @@
 package sort
 
-func swap(list []int, a, b int) {
-	list[a], list[b] = list[b], list[a]
-}
-
 func Bubble(list []int) []int {
 	for i := len(list) - 1; i > 1; i-- {
 		for j := 0; j < i; j++ {
---
+++ fundamentals/sorting/util.go
@@ -0,0 +1,5 @@
+package sort
+
+func swap(list []int, a, b int) {
+	list[a], list[b] = list[b], list[a]
+}
