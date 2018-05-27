99943b38766b79011fb5ec90b329f5e5854e04be
Philip Scott <pscott@zeptohost.com>
2018-05-27 14:27:02 -0400

    feat(why): Add insertion sort

---
+++ fundamentals/sorting/insertion.go
@@ -0,0 +1,22 @@
+package sort
+
+func shift_up(list []int, start, end int) {
+  for i := start; i < end; i++ {
+    list[i + 1] = list[i]
+  }
+}
+
+func Insertion(list []int) []int {
+  for i := 1; i < len(list); i++ {
+    for j := 0; j < i; j++ {
+      if list[j] > list[i] {
+        temp := list[i]
+        shift_up(list, j, i)
+        list[j] = temp
+        break
+      }
+    }
+  }
+
+  return list
+}
