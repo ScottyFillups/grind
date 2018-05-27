19f88dcdeea5eb06f16968beacef441d6eb5891e
Philip Scott <pscott@zeptohost.com>
2018-05-27 12:42:26 -0400

    go fmt

--- fundamentals/sorting/sort_test.go
+++ fundamentals/sorting/sort_test.go
@@ -1,20 +1,20 @@
 package sort
 
 import (
-  "github.com/stretchr/testify/assert"
-  "testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestSimple(t *testing.T) {
-  list := []int{1, 3, 5, 2, 6, 8, 4}
-  sorted := []int{1, 2, 3, 4, 5, 6, 8}
+	list := []int{1, 3, 5, 2, 6, 8, 4}
+	sorted := []int{1, 2, 3, 4, 5, 6, 8}
 
-  assert.Equal(t, sorted, Bubble(list))
+	assert.Equal(t, sorted, Bubble(list))
 }
 
 func TestDuplicate(t *testing.T) {
-  list := []int{22, 34, 65, 23, 34, 65, 65}
-  sorted := []int{22, 23, 34, 34, 65, 65, 65}
+	list := []int{22, 34, 65, 23, 34, 65, 65}
+	sorted := []int{22, 23, 34, 34, 65, 65, 65}
 
-  assert.Equal(t, sorted, Bubble(list))
+	assert.Equal(t, sorted, Bubble(list))
 }
